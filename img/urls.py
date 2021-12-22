6580a52c8d73435b63496cebc18ad39c74e8068e
Furkan.Ozdemir <ozthefur@gmail.com>
2021-12-22 16:24:25 +0300

    duplicate result minimized

--- img/urls.py
+++ img/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     # path("results", views.results, name="results"),
     url(r"^results/$", views.results, name="results"),
+    path("api_limit", views.api_limit, name="api_limit"),
 ]
--- img/views.py
+++ img/views.py
@@ -21,6 +21,10 @@ def index(request):
         language = request.POST["language"]
         query_amount = request.POST["query_amount"]
         # query_amount = "13"  # hard coded due to search api limit
+        sort_list = ["", "followers", "joined", "repositories"]
+        sort_index = randint(0, 3)
+        desc_asc_index = randint(0, 1)
+        desc_asc_list = ["desc", "asc"]
 
         for i in range(1, int(query_amount) // 10 + 1):
             github_url = (
@@ -31,6 +35,10 @@ def index(request):
                 + "&q="
                 + keywords
                 + "&type=Users"
+                + "&s="
+                + sort_list[sort_index]
+                + "&o="
+                + desc_asc_list[desc_asc_index]
             )
             r = requests.get(github_url)
             soup = BeautifulSoup(r.content, "html.parser")
@@ -39,26 +47,29 @@ def index(request):
             )  # [src]
             for url in profile_image_url_list:
                 headers = {"Authorization": "token " + TOKEN}
-                r = requests.get(
-                    "https://api.github.com/search/repositories?q=language:"
-                    + language
-                    + "+user:"
-                    + url["alt"].strip("@"),
-                    headers=headers,
-                )
-                repos = r.json()
-                print(repos["total_count"])
-                if not Github.objects.filter(alt=url["alt"]).exists():
-                    new_github = Github(
-                        image_url=url["src"].replace("s=40", "s=200"),
-                        github_url="https://github.com/" + url["alt"].strip("@"),
-                        title=keywords,
-                        language=language,
-                        added_date=datetime.now(tz=timezone.utc),
-                        alt=url["alt"],
-                        repo_count=int(repos["total_count"]),
+                try:
+                    r = requests.get(
+                        "https://api.github.com/search/repositories?q=language:"
+                        + language
+                        + "+user:"
+                        + url["alt"].strip("@"),
+                        headers=headers,
                     )
-                    new_github.save()
+                    repos = r.json()
+                    print(repos["total_count"])
+                    if not Github.objects.filter(alt=url["alt"]).exists():
+                        new_github = Github(
+                            image_url=url["src"].replace("s=40", "s=200"),
+                            github_url="https://github.com/" + url["alt"].strip("@"),
+                            title=keywords,
+                            language=language,
+                            added_date=datetime.now(tz=timezone.utc),
+                            alt=url["alt"],
+                            repo_count=int(repos["total_count"]),
+                        )
+                        new_github.save()
+                except KeyError:
+                    return redirect("/api_limit")
 
         return redirect(
             reverse("results")
@@ -77,7 +88,9 @@ def results(request):
     keywords = request.GET.get("keywords", "")
     language = request.GET.get("language", "")
     query_amount = request.GET.get("query_amount", "")
-    people = Github.objects.filter(title=keywords, language=language)
+    people = Github.objects.filter(title=keywords, language=language).order_by(
+        "-added_date"
+    )
 
     return render(
         request,
@@ -86,3 +99,7 @@ def results(request):
             "people": people[: int(query_amount)],
         },
     )
+
+
+def api_limit(request):
+    return render(request, "api_limit.html")
